ae58d45b15a28d746524cee1ecd490567c22ebd8
Salavat- <salavat-kadyrov@bk.ru>
2022-03-03 22:01:28 +0300

    add viewBinding

--- app/src/main/java/com/prod/goodweather/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/prod/goodweather/ui/fragment/HomeFragment.kt
@@ -1,8 +1,13 @@
 package com.prod.goodweather.ui.fragment
 
 import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.prod.goodweather.databinding.FragmentHomeBinding
 import com.prod.goodweather.ui.GoodWeatherApp
 import com.prod.goodweather.ui.viewModel.HomeFragmentViewModel
 import com.prod.goodweather.ui.viewModel.ViewModelFactory
@@ -16,12 +21,29 @@ class HomeFragment : Fragment() {
     @Inject
     lateinit var viewModelFactory: ViewModelFactory
     private val viewModel: HomeFragmentViewModel by lazy {
-        ViewModelProvider(this,viewModelFactory)[HomeFragmentViewModel::class.java]
+        ViewModelProvider(this, viewModelFactory)[HomeFragmentViewModel::class.java]
     }
 
+    private var _binding: FragmentHomeBinding? = null
+    val binding
+        get() = _binding ?: throw RuntimeException("FragmentHomeBinding is null")
 
     override fun onAttach(context: Context) {
         component.inject(this)
         super.onAttach(context)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentHomeBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
